c265b3c6c4b8ddc0f27aa15f43eb447b8302b1ad
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-12 11:41:33 -0500

    team member info pulling and replacing correctly

--- team.js
+++ team.js
@@ -1,4 +1,7 @@
 const teamCardsHolder = document.querySelector(".team-cards");
+const houstonButton = document.querySelector("#houston");
+const atlantaButton = document.querySelector("#atlanta");
+const tampaButton = document.querySelector("#tampa");
 
 const aboutMeSnippits = [
   "I want you looking fly!",
@@ -23,9 +26,9 @@ const jobTitles = [
 ];
 
 const getTeamInfo = async () => {
-  //   if (teamCardsHolder !== 0) {
-  //     teamCardsHolder.innerHTML = "";
-  //   }
+  if (teamCardsHolder !== 0) {
+    teamCardsHolder.innerHTML = "";
+  }
   const allTeamMembers = await fetch(
     "https://randomuser.me/api/?inc=name,picture&results=8&nat=us"
   );
@@ -56,4 +59,48 @@ const getTeamInfo = async () => {
   }
 };
 
-getTeamInfo();
+const getTampaTeamInfo = async () => {
+  if (teamCardsHolder !== 0) {
+    teamCardsHolder.innerHTML = "";
+  }
+  const allTeamMembers = await fetch(
+    "https://randomuser.me/api/?inc=name,picture&results=8&nat=us"
+  );
+  const teamMembersForSite = await allTeamMembers.json();
+  for (let i = 0; i < 6; i++) {
+    const teamMemberCard = document.createElement("div");
+    const teamMemberImage = document.createElement("img");
+    const teamMemberName = document.createElement("h3");
+    const teamMemberTitle = document.createElement("h4");
+    const firstName = teamMembersForSite.results[i].name.first;
+    const lastName = teamMembersForSite.results[i].name.last;
+    const image = teamMembersForSite.results[i].picture.large;
+    const fullName = `${firstName} ${lastName}`;
+    teamMemberName.innerHTML = fullName;
+    teamMemberImage.src = image;
+    const teamMemberAboutMe = document.createElement("h4");
+    const jobTitle = jobTitles[i];
+    const aboutMe = aboutMeSnippits[i];
+    teamMemberAboutMe.innerHTML = aboutMe;
+    teamMemberTitle.innerHTML = jobTitle;
+    teamMemberCard.append(
+      teamMemberImage,
+      teamMemberName,
+      teamMemberTitle,
+      teamMemberAboutMe
+    );
+    teamCardsHolder.append(teamMemberCard);
+  }
+};
+
+houstonButton.addEventListener("click", (e) => {
+  getTeamInfo();
+});
+
+atlantaButton.addEventListener("click", (e) => {
+  getTeamInfo();
+});
+
+tampaButton.addEventListener("click", (e) => {
+  getTampaTeamInfo();
+});
