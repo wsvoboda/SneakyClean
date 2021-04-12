cb611f60327d65db1d8aa1e1233e00b9178ee308
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-12 17:23:02 -0500

    added location search for each location

--- locations.js
+++ locations.js
@@ -36,25 +36,75 @@
 //   });
 // }
 
-const zipInputBox = document.querySelector(".zip-code");
-const zipSearchButton = document.querySelector(".zip-search-button");
-const zipResponse = document.querySelector(".zip-query-answer");
+const houstonZipInputBox = document.querySelector(".houston-zip-code");
+const houstonZipSearchButton = document.querySelector(
+  ".houston-zip-search-button"
+);
+const houstonZipResponse = document.querySelector(".houston-zip-query-answer");
 
 const checkZipHouston = async () => {
   const distanceArray = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/5bHSHbDtrU53Uoo7b1f2izlJu31yIw3HynfGvj7GnM6VwwWWbto1Ahv4XyMCouW8/distance.json/77043/${zipInputBox.value}/mile`
+    `https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/5bHSHbDtrU53Uoo7b1f2izlJu31yIw3HynfGvj7GnM6VwwWWbto1Ahv4XyMCouW8/distance.json/77043/${houstonZipInputBox.value}/mile`
   );
   const distanceValue = await distanceArray.json();
   const distanceToLocation = distanceValue.distance; // gives distance in miles
   if (distanceToLocation <= 50) {
-    zipResponse.innerHTML = "We service your area!";
+    houstonZipResponse.innerHTML = "We service your area!";
   } else if (distanceToLocation > 50) {
-    zipResponse.innerHTML = "We don't yet service your area.";
+    houstonZipResponse.innerHTML = "We don't service your area.";
   } else {
-    zipResponse.innerHTML = "Please enter a valid zip code";
+    houstonZipResponse.innerHTML = "Please enter a valid zip code";
   }
 };
 
-zipSearchButton.addEventListener("click", (e) => {
+houstonZipSearchButton.addEventListener("click", (e) => {
   checkZipHouston();
+});
+
+const atlantaZipInputBox = document.querySelector(".atlanta-zip-code");
+const atlantaZipSearchButton = document.querySelector(
+  ".atlanta-zip-search-button"
+);
+const atlantaZipResponse = document.querySelector(".atlanta-zip-query-answer");
+
+const checkZipAtlanta = async () => {
+  const distanceArray = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/5bHSHbDtrU53Uoo7b1f2izlJu31yIw3HynfGvj7GnM6VwwWWbto1Ahv4XyMCouW8/distance.json/30305/${atlantaZipInputBox.value}/mile`
+  );
+  const distanceValue = await distanceArray.json();
+  const distanceToLocation = distanceValue.distance; // gives distance in miles
+  if (distanceToLocation <= 50) {
+    atlantaZipResponse.innerHTML = "We service your area!";
+  } else if (distanceToLocation > 50) {
+    atlantaZipResponse.innerHTML = "We don't service your area.";
+  } else {
+    atlantaZipResponse.innerHTML = "Please enter a valid zip code";
+  }
+};
+
+atlantaZipSearchButton.addEventListener("click", (e) => {
+  checkZipAtlanta();
+});
+
+const tampaZipInputBox = document.querySelector(".tampa-zip-code");
+const tampaZipSearchButton = document.querySelector(".tampa-zip-search-button");
+const tampaZipResponse = document.querySelector(".tampa-zip-query-answer");
+
+const checkZipTampa = async () => {
+  const distanceArray = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/5bHSHbDtrU53Uoo7b1f2izlJu31yIw3HynfGvj7GnM6VwwWWbto1Ahv4XyMCouW8/distance.json/33602/${tampaZipInputBox.value}/mile`
+  );
+  const distanceValue = await distanceArray.json();
+  const distanceToLocation = distanceValue.distance; // gives distance in miles
+  if (distanceToLocation <= 50) {
+    tampaZipResponse.innerHTML = "We service your area!";
+  } else if (distanceToLocation > 50) {
+    tampaZipResponse.innerHTML = "We don't service your area.";
+  } else {
+    tampaZipResponse.innerHTML = "Please enter a valid zip code";
+  }
+};
+
+tampaZipSearchButton.addEventListener("click", (e) => {
+  checkZipTampa();
 });
