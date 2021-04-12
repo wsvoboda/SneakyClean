a614ff79427439d3a4fa42a96df3f1a1960fd65f
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-12 14:17:45 -0500

    embedded maps with markers

---
+++ locations.js
@@ -0,0 +1,35 @@
+function initMap() {
+  const sneakyCleanHouston = { lat: 29.795, lng: -95.569 };
+  const houstonMap = new google.maps.Map(
+    document.getElementById("houston-map"),
+    {
+      zoom: 12,
+      center: sneakyCleanHouston,
+    }
+  );
+  const houstonMarker = new google.maps.Marker({
+    position: sneakyCleanHouston,
+    map: houstonMap,
+  });
+  const sneakyCleanAtlanta = { lat: 33.848, lng: -84.373 };
+  const atlantaMap = new google.maps.Map(
+    document.getElementById("atlanta-map"),
+    {
+      zoom: 12,
+      center: sneakyCleanAtlanta,
+    }
+  );
+  const atlantaMarker = new google.maps.Marker({
+    position: sneakyCleanAtlanta,
+    map: atlantaMap,
+  });
+  const sneakyCleanTampa = { lat: 27.947, lng: -82.452 };
+  const tampaMap = new google.maps.Map(document.getElementById("tampa-map"), {
+    zoom: 12,
+    center: sneakyCleanTampa,
+  });
+  const tampaMarker = new google.maps.Marker({
+    position: sneakyCleanTampa,
+    map: tampaMap,
+  });
+}
