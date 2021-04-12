9ed781e92fc548020e0eb5aaecb918823c1cc6bb
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-12 17:52:45 -0500

    alert with order confirmation number working

--- services.js
+++ services.js
@@ -1,2 +1,12 @@
 const form = document.querySelector("form");
-console.log(form);
+const confirmation = document.querySelector("#confirmation");
+
+const takeOrder = () => {
+  const randomNumber = Math.floor(100000 + Math.random() * 999999);
+  confirmation.innerHTML = `Thank you for your order! Your confirmation number is #${randomNumber}. We will be in touch soon to schedule delivery and pickup!`;
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  takeOrder();
+});
