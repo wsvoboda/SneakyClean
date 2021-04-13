503139306ece472ae89098d665d42f44dc52f19d
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-13 17:00:05 -0500

    added all form components

--- services.js
+++ services.js
@@ -2,8 +2,8 @@ const form = document.querySelector("form");
 const confirmation = document.querySelector("#confirmation");
 
 const takeOrder = () => {
-  const randomNumber = Math.floor(100000 + Math.random() * 999999);
-  confirmation.innerHTML = `Thank you for your order! Your confirmation number is #${randomNumber}. We will be in touch soon to schedule pickup and delivery!`;
+  const randomNumber = Math.floor(10000 + Math.random() * 99999);
+  confirmation.innerHTML = `Thank you for your order! Your confirmation number is #00${randomNumber}. We will be in touch soon to schedule pickup and delivery!`;
 };
 
 form.addEventListener("submit", (e) => {
