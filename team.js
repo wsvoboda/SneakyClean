0fb7a65c0956b3545b71d62d3af0f055149ce5c9
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-13 19:22:23 -0500

    give flex layout to team page

--- team.js
+++ team.js
@@ -1,4 +1,4 @@
-const teamCardsHolder = document.querySelector(".team-cards");
+const teamCardsHolder = document.querySelector("#team-cards");
 const houstonButton = document.querySelector("#houston");
 const atlantaButton = document.querySelector("#atlanta");
 const tampaButton = document.querySelector("#tampa");
@@ -24,40 +24,6 @@ const jobTitles = [
   "Restoration Engineer",
   "Restoration Engineer",
 ];
-
-// const getTeamInfo = async () => {
-//   if (teamCardsHolder !== 0) {
-//     teamCardsHolder.innerHTML = "";
-//   }
-//   const allTeamMembers = await fetch(
-//     "https://randomuser.me/api/?inc=name,picture&results=8&nat=us"
-//   );
-//   const teamMembersForSite = await allTeamMembers.json();
-//   for (let i = 0; i < 8; i++) {
-//     const teamMemberCard = document.createElement("div");
-//     const teamMemberImage = document.createElement("img");
-//     const teamMemberName = document.createElement("h3");
-//     const teamMemberTitle = document.createElement("h4");
-//     const firstName = teamMembersForSite.results[i].name.first;
-//     const lastName = teamMembersForSite.results[i].name.last;
-//     const image = teamMembersForSite.results[i].picture.large;
-//     const fullName = `${firstName} ${lastName}`;
-//     teamMemberName.innerHTML = fullName;
-//     teamMemberImage.src = image;
-//     const teamMemberAboutMe = document.createElement("h4");
-//     const jobTitle = jobTitles[i];
-//     const aboutMe = aboutMeSnippits[i];
-//     teamMemberAboutMe.innerHTML = aboutMe;
-//     teamMemberTitle.innerHTML = jobTitle;
-//     teamMemberCard.append(
-//       teamMemberImage,
-//       teamMemberName,
-//       teamMemberTitle,
-//       teamMemberAboutMe
-//     );
-//     teamCardsHolder.append(teamMemberCard);
-//   }
-// };
 
 const getTeamInfo = async () => {
   if (teamCardsHolder !== 0) {
