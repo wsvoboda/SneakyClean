04adacc5e3c042dfe08342344276714656885ffc
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-14 14:20:37 -0500

    form response customized with name

--- services.js
+++ services.js
@@ -1,13 +1,14 @@
 const form = document.querySelector("#order-form");
 const confirmation = document.querySelector("#confirmation");
+const customerName = document.querySelector("#firstname");
 
 const takeOrder = () => {
   const randomNumber = Math.floor(10000 + Math.random() * 99999);
-  confirmation.innerHTML = `Thank you for your order! Your confirmation number is #00${randomNumber}. We will be in touch soon to schedule pickup and delivery!`;
+  confirmation.innerHTML = `Thank you for your order, ${customerName.value}! Your confirmation number is #00${randomNumber}. We will be in touch soon to schedule pickup and delivery!`;
 };
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  form.reset();
   takeOrder();
+  form.reset();
 });
